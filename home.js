6a15b96a8cb1fe9834d2a10a25c59daa6715da84
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-01-22 18:16:59 +0100

    piccole modifiche

--- home.js
+++ home.js
@@ -1,11 +1,86 @@
-let qeen = 'playlist'
+const search = document.getElementById("search");
+const searchBar = document.getElementById("searchBarDiv");
+const form = document.getElementById("formSearch");
+const formInput = document.getElementById("formInput");
 
-fetch(' https://striveschool-api.herokuapp.com/api/deezer/search?q=' + qeen)
-  .then((response) => {
-    if (response.ok) {
-      return response.json()
-    }
-  })
-  .then((dati) => {
-    console.log(dati)
-  })
+const dNone = () => {
+  searchBar.classList.toggle("d-none");
+};
+search.addEventListener("click", () => {
+  dNone();
+});
+
+let params = "";
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   params = formInput.value;
+//   fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=" + params)
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//     })
+//     .then((dati) => {
+//       console.log(dati);
+//       formInput.value = "";
+//     });
+// });
+
+// const makeAPICall = (searchValue) => {
+//   if (!searchValue) {
+//     return;
+//   }
+//   params = searchValue;
+//   fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=" + params)
+//     .then((res) => {
+//       return res.json().then((response) => {
+//         console.log(response);
+
+//         console.log("ciao");
+//       });
+//     })
+//     .catch((e) => {});
+// };
+
+// const debounce = (fn, delay = 1000) => {
+//   let timerId = null;
+//   return (...args) => {
+//     clearTimeout(timerId);
+//     timerId = setTimeout(() => fn(...args), delay);
+//   };
+// };
+
+// const onInput = debounce(makeAPICall, 2000);
+
+// formInput.addEventListener("input", (e) => {
+//   makeAPICall(e.target.value);
+// });
+
+const searchData = (event) => {
+  const value = event.target.value;
+  console.log("Searching...", value);
+  fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=" + value)
+    .then((res) => {
+      return res.json().then((response) => {
+        console.log(response);
+
+        formInput.value = "";
+        dNone();
+      });
+    })
+    .catch((e) => {});
+};
+
+const debounce = (callback, waitTime) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, waitTime);
+  };
+};
+
+const debounceHandler = debounce(searchData, 1000);
+formInput.addEventListener("input", debounceHandler);
