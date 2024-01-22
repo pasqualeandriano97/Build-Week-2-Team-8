493eb3ac43d0faeb3bbf024731504311ae9d6371
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-01-22 19:26:27 +0100

    modifiche

--- home.js
+++ home.js
@@ -6,56 +6,12 @@ const formInput = document.getElementById("formInput");
 const dNone = () => {
   searchBar.classList.toggle("d-none");
 };
-search.addEventListener("click", () => {
+search.addEventListener("click", (e) => {
+  e.preventDefault();
   dNone();
 });
 
 let params = "";
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   params = formInput.value;
-//   fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=" + params)
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//     })
-//     .then((dati) => {
-//       console.log(dati);
-//       formInput.value = "";
-//     });
-// });
-
-// const makeAPICall = (searchValue) => {
-//   if (!searchValue) {
-//     return;
-//   }
-//   params = searchValue;
-//   fetch(" https://striveschool-api.herokuapp.com/api/deezer/search?q=" + params)
-//     .then((res) => {
-//       return res.json().then((response) => {
-//         console.log(response);
-
-//         console.log("ciao");
-//       });
-//     })
-//     .catch((e) => {});
-// };
-
-// const debounce = (fn, delay = 1000) => {
-//   let timerId = null;
-//   return (...args) => {
-//     clearTimeout(timerId);
-//     timerId = setTimeout(() => fn(...args), delay);
-//   };
-// };
-
-// const onInput = debounce(makeAPICall, 2000);
-
-// formInput.addEventListener("input", (e) => {
-//   makeAPICall(e.target.value);
-// });
 
 const searchData = (event) => {
   const value = event.target.value;
